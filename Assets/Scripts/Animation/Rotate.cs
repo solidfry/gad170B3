a76b957d54571625ac9861a643c2832a7122c6e3
solidfry <ellioteweb@gmail.com>
2022-04-18 20:36:24 +1000

    rotater

--- Assets/Scripts/Animation/Rotate.cs
+++ Assets/Scripts/Animation/Rotate.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
 
     public float rotateSpeed = 5f;
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up, Time.deltaTime * rotateSpeed);
